42d616dfaca81b780b9d5ad437a085adb2347ae7
eunoiaOIO <im.yeunn@gmail.com>
2022-07-06 15:53:44 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import MainPage from "./pages/Main";
-
+import MarketPage from "./pages/Market";
 function App() {
   return (
     <Routes>
     <Route path='/' element={<MainPage/>} ></Route>
+    <Route path='/market' element={<MarketPage/>} ></Route>
     {/* <Route path='/detailpages/*' >
       <Route path=":id" element={<DetailPage />} />
     </Route>
---
+++ src/pages/Market.js
@@ -0,0 +1,12 @@
+
+
+const MarketPage = () => {
+
+
+    return(
+        <div>hello world</div>
+    );
+
+}
+
+export default MarketPage;
