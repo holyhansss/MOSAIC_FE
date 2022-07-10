9b5dc28eeb54a071b49a0f81beceec6e810c53c4
한성원 <holyhansss@gmail.com>
2022-07-10 22:30:57 +0900

    idk

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,18 @@
-
+import {Button} from "react-bootstrap";
 
 const MainPage = () => {
 
 
     return(
-        <div>hello world</div>
+    <>
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+        <Button variant="link">Link</Button>
+      </>
     );
 
 }
