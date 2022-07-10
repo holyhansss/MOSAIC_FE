438cdbfbad5158d8367e559cde9ae13ded88d641
한성원 <holyhansss@gmail.com>
2022-07-10 22:38:29 +0900

    test

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import {Button} from "react-bootstrap";
 
-=======
->>>>>>> d54dbc24d71f7b20bdf6c094253ea729b93b7f51
 const MainPage = () => {
+
+
     return(
-<<<<<<< HEAD
     <>
         <Button variant="primary">Primary</Button>{' '}
         <Button variant="secondary">Secondary</Button>{' '}
@@ -15,9 +13,6 @@ const MainPage = () => {
         <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
         <Button variant="link">Link</Button>
       </>
-=======
-        <div>Main</div>
->>>>>>> d54dbc24d71f7b20bdf6c094253ea729b93b7f51
     );
 
 }
