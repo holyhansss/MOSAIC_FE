613d02698d103ccd82529fac8d4431c999b8ca66
Heeeera <chr5487@naver.com>
2022-07-27 16:14:51 +0900

    Add styled-components in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,106 @@
 import React from "react";
-import { Container, Col, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import styled from 'styled-components'
 import { logout } from '../../firebase';
 
-function Header({ user, admin }) {
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 15vh;
+    background-color: navy;
+`;
 
+const Navbar = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 70%;
+    height: 80%;
+    border: 1px solid white;
+    border-radius: 20px;
+`;
+
+const NavSideContainer = styled.div`
+    width: 30%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const NavCenterContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 40%;
+    height: 100%
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const NavMain = styled.div`
+    color: white;
+    font-size: 1.5em;
+`;
+
+const NavMenuContainer = styled.div`
+    width: 33%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const NavMenu = styled.div`
+    color: white;
+`;
+
+const NavProfile = styled.img`
+    border-radius: 50%;
+`;
+
+
+function Header({ user, admin }) {
     return (
-        <>
-            <Navbar bg="light" variant="light">
-                <Container>
-                    <Col lg="4">
-                        <Navbar.Brand href="/">MOSAIC</Navbar.Brand>
-                    </Col>
-                    <Col lg="4" md="6">
-                        <Nav className="justify-content-around"> 
-                            <Link to="/">홈</Link>
-                            <Link to="/market">시장 동향</Link>  
-                            <Link to="/reportList">리포트</Link>
-                        </Nav>
-                    </Col>
-                    <Col lg="4" >
-                        <Nav className="justify-content-end"> 
-                            {
-                                user !== null ? 
-                                (
-                                    <NavDropdown title={<img alt="profile" style={{borderRadius:'50%'}} src={user.photoURL} width="30" height="30"/>} id="basic-nav-dropdown">
-                                        <NavDropdown.Item><Link to="/profile">마이페이지</Link></NavDropdown.Item>
-                                        {
-                                            admin === true &&
-                                            <Link to="admin"> 관리자</Link>
-                                        }
-                                        <NavDropdown.Item onClick={logout}>로그아웃</NavDropdown.Item>
-                                    </NavDropdown>
-                                ) :
-                                <Link to="/login">로그인</Link>
-                            }
-                        </Nav>
-                    </Col>
-                </Container>
+        <Container>
+            <Navbar>
+                <NavSideContainer>
+                    <Link to="/"><NavMain>MOSAIC</NavMain></Link>
+                </NavSideContainer>
+                <NavCenterContainer>
+                    <NavMenuContainer>
+                        <Link to="/"><NavMenu>홈</NavMenu></Link>
+                    </NavMenuContainer>
+                    <NavMenuContainer>
+                        <Link to="/market"><NavMenu>시장 동향</NavMenu></Link>
+                    </NavMenuContainer>
+                    <NavMenuContainer>
+                        <Link to="/reportList"><NavMenu>리포트</NavMenu></Link>
+                    </NavMenuContainer>
+                </NavCenterContainer>
+                <NavSideContainer>
+                    {
+                        user !== null ? 
+                        (
+                            <NavDropdown
+                                menuVariant="dark"
+                                title={<img alt="profile" style={{borderRadius:'50%'}} src={user.photoURL} width="30" height="30"/>}
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item><Link to="/profile">마이페이지</Link></NavDropdown.Item>
+                                {
+                                    admin === true &&
+                                    <NavDropdown.Item><Link to="admin"> 관리자</Link></NavDropdown.Item>
+                                }
+                                <NavDropdown.Item onClick={logout}>로그아웃</NavDropdown.Item>
+                            </NavDropdown>
+                        ) :
+                        <Link to="/login">로그인</Link>
+                    }
+                </NavSideContainer>
             </Navbar>
-        </>
+        </Container>
     );
 }
 
