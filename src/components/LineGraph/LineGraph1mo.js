94f4a65a366c8af5e62eb1c21cfe051cd3a8b125
eunoiaOIO <im.yeunn@gmail.com>
2022-07-27 16:27:29 +0900

    그래프 오류 해결

--- src/components/LineGraph/LineGraph1mo.js
+++ src/components/LineGraph/LineGraph1mo.js
@@ -10,10 +10,8 @@ import { type } from '@testing-library/user-event/dist/type';
 // S&P 500 지수 와 CMC 200 지수를 그래프로(한달)
 
 function Index1mo() {
-  const axios = require('axios');
   const [time, setTime] = useState([]);
-  const [index1, setIndex1] = useState([]);
-  const [index2, setIndex2] = useState([]);
+  const [res, setRes] = useState([]);
 
   var SNPOptions = {
     method: 'GET',
@@ -43,32 +41,24 @@ function Index1mo() {
     }
   };
   
-  useEffect(() => {
-    axios.request(SNPOptions)
+  async function getSNP() {
+    let returnValue;
+    await axios.request(SNPOptions)
     .then((res: any) => {
-
-
         const IndexData = res.data.chart.result[0].indicators.quote[0].close.map((data, index) => (
           data && {
             time:moment(res.data.chart.result[0].timestamp[index]*1000).format('MM월DD일'),
             SnP: data
-        
-            
           }))
-          
         var SNP_first=IndexData[0].SnP
         for (let i=0;i<IndexData.length; i++){
-
           if (IndexData[0] == null ){
-                
                 for (let j = 0; ; j++) {
                   if (IndexData[j] == null)
                     continue;
                   IndexData[0]= IndexData[j];
                   break;
-                }
-                
-                
+                } 
             }
             else if (IndexData[i] == null  ) {
               IndexData[i]= {time: null, SnP: null};
@@ -79,40 +69,33 @@ function Index1mo() {
             else{  
             };  
         }
-
-        for (const item of IndexData){
-                
+      for (const item of IndexData){    
             item.SnP=100/SNP_first*item.SnP
-   
-            }
-            
-            
-            setIndex1(IndexData)
-            
+        }
+        returnValue = IndexData
       });
-      axios.request(CMCOptions)
+    return returnValue
+  };
+
+  async function getCMC() {
+    let returnValue;
+    await axios.request(CMCOptions)
       .then((res: any) => {
           const IndexData2 = res.data.chart.result[0].indicators.quote[0].close.map((data, index) => (
             data && {
               time:res.data.chart.result[0].timestamp[index],
               CMC: data
-          
-              
             }))
   
           var CMC_first=IndexData2[0].CMC
           for (let i=0;i<IndexData2.length; i++){
-
             if (IndexData2[0] == null ){
-                  
                   for (let j = 0; ; j++) {
                     if (IndexData2[j] == null)
                       continue;
                     IndexData2[0]= IndexData2[j];
                     break;
-                  }
-                  
-                  
+                  }   
               }
               else if (IndexData2[i] == null  ) {
                 IndexData2[i]= {time: null, CMC: null};
@@ -123,31 +106,37 @@ function Index1mo() {
               else{  
               };  
           }
-          
-          for (const item of IndexData2){
-                  
-              item.CMC=100/CMC_first*item.CMC
-     
-              }
-              
-              
-              setIndex2(IndexData2)
-              
-        });
-    }, []);
-    const NewIndex =(data1, data2) => {
-      const res=[];
+    for (const item of IndexData2){         
+        item.CMC=100/CMC_first*item.CMC
+        }
+        returnValue = IndexData2
+      });
+    
+    return returnValue
+  };
+    useEffect(() => {       
+          NewIndex();
+        }, []);
       
-      for (let i =0; i< data1.length; i++){
-        res.push({
-          time:data1[i].time,
-          SnP:data1[i].SnP,
-          CMC:data2[i].CMC
-        });
-      }
-      return res;
-    };
-    const index3=NewIndex(index1,index2);
+      const NewIndex = async() => {
+          const resTemp=[];
+          const data1 = await getSNP();
+          const data2 = await getCMC();
+          console.log("data1:", data1);
+          console.log("data2:",data2);
+          
+          for (let i =0; i< data1.length; i++){
+            
+            resTemp.push({
+              time:data1[i].time,
+              SnP:data1[i].SnP,
+              CMC:data2[i].CMC,
+            });
+          }
+          console.log("res: ", res);
+          setRes(resTemp)
+          return res;
+       };
   return(
 
     
@@ -155,12 +144,12 @@ function Index1mo() {
       <div><h>S&P 500 vs CMC200  1 Month ver </h></div>
 
     { 
-      index3 &&
+      res &&
       <div>
       <LineChart
         width={900}
         height={300}
-        data={index3}
+        data={res}
         margin={{top: 5, right: 20, left: 20, bottom: 5}}
       >
         <CartesianGrid vertical={false} />
