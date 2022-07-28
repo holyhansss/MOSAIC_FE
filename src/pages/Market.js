4041dd522ffeffe0126ae1f7463c8c30eafe3181
yea <yegoun98@gmail.com>
2022-07-28 11:05:33 +0900

    Jun 28 call category data when click each category button. Added CategoryLineGraph/ and changed Market.js.

--- src/pages/Market.js
+++ src/pages/Market.js
@@ -1,5 +1,4 @@
-import React ,{useState, useEffect}from "react";
-
+import React ,{useState}from "react";
 import FearandGreed from "../components/FearAndGreed/FearandGreed.js";
 import Index1d from "../components/SnPCMCLineGraph/LineGraph1d.js";
 import Index1mo from "../components/SnPCMCLineGraph/LineGraph1mo.js";
@@ -7,7 +6,9 @@ import Index1y from "../components/SnPCMCLineGraph/LineGraph1y.js";
 import CategoryLineGraph_1y from "../components/CategoryLineGraph/CategoryLineGraph_1y.js"
 import CategoryLineGraph_1mo from "../components/CategoryLineGraph/CategoryLineGraph_1mo.js"
 import CategoryLineGraph_1d from "../components/CategoryLineGraph/CategoryLineGraph_1d.js"
-import CategoryButton from "../components/CategoryLineGraph/CategoryButton.js"
+import axios from 'axios';
+
+// import CategoryButton from "../components/CategoryLineGraph/CategoryButton.js"
 // 시장동향
 // S&P 500 지수와 CMC 200 그래프(line) 불러옴
 //공포탐욕지수 이미지 불러옴
@@ -18,17 +19,10 @@ function Marketpage()
 {
   const range = ["1d","1mo", "1y"];
   const [content, setContent] = useState(null);
-  const [dateRange, setDateRange] = useState("");
+  const [dateRange, setDateRange] = useState("1y");
   const [categoryIndex, setCategoryIndex] = useState(null);
   const [categoryArray, setCategoryArray] = useState([true,true,true,true,true]);
   const [props, setProps] = useState({})
-  // let props = {
-  //   imageUrl:"/js.com",
-  //   imageText:""food""
-  //   }
-  //   <ImageText {...props} />
-
-
 
   const buttonValueSetting = e => {
       e.preventDefault();
@@ -44,62 +38,117 @@ function Marketpage()
   const buttonCategorySettings = e => {
     e.preventDefault();
     if (e.target.name === "1y") { 
-      console.log("1d clicked!"); 
+      console.log("1y clicked!"); 
+      setDateRange("1y")
       setProps( {
         dateRange: dateRange,
         categoryArray: categoryArray
       })
-      setDateRange("1y")
       setCategoryIndex(0)
+
     } else if (e.target.name === "1mo") {
       console.log("1mo clicked!");
-      setProps( {
-        dateRange: dateRange,
-        categoryArray: categoryArray
-      })
       setDateRange("1mo")
-      setCategoryIndex(1)
-    } else if (e.target.name === "1d") {
-      console.log("1y clicked!");
       setProps( {
         dateRange: dateRange,
         categoryArray: categoryArray
       })
+      setCategoryIndex(1)
+
+    } else if (e.target.name === "1d") {
+      console.log("1d clicked!");
       setDateRange("1d")
+      setProps( {
+        dateRange: dateRange,
+        categoryArray: categoryArray
+      })
       setCategoryIndex(2)
     }
   }
 
-  // const buttonToggleArrayElement = e => {
-  //   e.preventDefault();
-  //   const arrayToReplace = [...categoryArray];
-  //   if (e.target.name == "Currency" ){
-  //     arrayToReplace[0] = !arrayToReplace[0];
-  //     setCategoryArray(arrayToReplace);
-  //   }else if (e.target.name == "Smart Contract Platform" ){
-  //     arrayToReplace[1] = !arrayToReplace[1];
-  //     setCategoryArray(arrayToReplace);
-  //   }else if (e.target.name == "Computing" ){
-  //     arrayToReplace[2] = !arrayToReplace[2];
-  //     setCategoryArray(arrayToReplace);
-  //   }else if (e.target.name == "DeFi" ){
-  //     arrayToReplace[3] = !arrayToReplace[3];
-  //     setCategoryArray(arrayToReplace);
-  //   }else if (e.target.name == "Culture & Entertainment" ){
-  //     arrayToReplace[4] = !arrayToReplace[4];
-  //     setCategoryArray(arrayToReplace);
-  //   } else {
-  //     console.log("wrong category name '" + e.target.name + "' at buttonAddCategoryArray");
-  //   }
-  //   setProps( {
-  //     ...props,
-  //     dateRange: dateRange,
-  //     categoryArray: categoryArray
-  //   })
-  //   console.log("props checkwise:", props);
-  //   console.log("props dateRange checkwise:", props.dateRange);
-  //   console.log("props categoryArray checkwise:", props.categoryArray);
-  // }
+  const CategoryButton = () => {
+    const buttonToggleArrayElement = e => {
+      e.preventDefault();
+      const arrayToReplace = [...categoryArray];
+      if (e.target.name === "Currency" ){
+        arrayToReplace[0] = !arrayToReplace[0];
+      }else if (e.target.name === "Smart Contract Platform" ){
+        arrayToReplace[1] = !arrayToReplace[1];
+      }else if (e.target.name === "Computing" ){
+        arrayToReplace[2] = !arrayToReplace[2];
+      }else if (e.target.name === "DeFi" ){
+        arrayToReplace[3] = !arrayToReplace[3];
+      }else if (e.target.name === "Culture & Entertainment" ){
+        arrayToReplace[4] = !arrayToReplace[4];
+      } else {
+        console.log("wrong category name '" + e.target.name + "' at buttonAddCategoryArray");
+      }
+      setCategoryArray(arrayToReplace);
+      if (categoryIndex === 0)
+        setCategoryIndex(3);
+      else if (categoryIndex === 3) {
+        setCategoryIndex(0);
+      } else if (categoryIndex === 1)
+        setCategoryIndex(4);
+      else if (categoryIndex === 4){
+        setCategoryIndex(1);
+      } else if (categoryIndex === 2)
+        setCategoryIndex(5);
+      else if (categoryIndex === 5){
+        setCategoryIndex(2);
+      } else {
+        console.error("Wrong categoryIndex; out of bounds");
+      }
+  
+
+      // console.log("Category Index:", categoryIndex);  
+      setProps( {
+        ...props,
+        dateRange: dateRange,
+        categoryArray: arrayToReplace
+      })
+
+      console.log("props categoryIndex checkwise:", categoryIndex);
+      console.log("props dateRange checkwise:", props.dateRange);
+      console.log("props categoryArray checkwise:", props.categoryArray);
+    }
+  
+  return (
+    <div>
+    {
+      categoryArray[0] ? (
+        <button onClick={buttonToggleArrayElement} name = "Currency" color="green"> Currency hide </button>
+        ):(
+          <button onClick={buttonToggleArrayElement} name= "Currency" color="green">Currency show </button>
+        )
+      } {
+      categoryArray[1] ? (
+        <button onClick={buttonToggleArrayElement} name = "Smart Contract Platform" theme="grey">Smart Contract Platform hide </button>
+        ):(
+          <button onClick={buttonToggleArrayElement} name= "Smart Contract Platform" theme="grey">Smart Contract Platform show </button>
+        )
+    }{
+      categoryArray[2] ? (
+        <button onClick={buttonToggleArrayElement} name = "Computing" theme="skyblue">Computing hide </button>
+        ):(
+          <button onClick={buttonToggleArrayElement} name= "Computing" theme="skyblue">Computing show </button>
+        )
+    }{
+      categoryArray[3] ? (
+        <button onClick={buttonToggleArrayElement} name = "DeFi" theme="pink">DeFi hide </button>
+        ):(
+          <button onClick={buttonToggleArrayElement} name= "DeFi" theme="pink">DeFi show </button>
+        )
+    }{
+      categoryArray[4] ? (
+        <button onClick={buttonToggleArrayElement} name = "Culture & Entertainment" theme="orange">Culture & Entertainment hide </button>
+        ):(
+          <button onClick={buttonToggleArrayElement} name= "Culture & Entertainment" theme="orange">Culture & Entertainment show </button>
+        )
+    }
+    </div> 
+  )
+}
   
   const selectComponent = [
       <Index1d />,
@@ -110,7 +159,7 @@ function Marketpage()
   const categoryComponent = [
     <CategoryLineGraph_1y {...props}/>,
     <CategoryLineGraph_1mo {...props}/>,
-    <CategoryLineGraph_1d {...props}/>
+    <CategoryLineGraph_1d {...props}/>,
   ]
   
     return (
@@ -141,43 +190,18 @@ function Marketpage()
           <button onClick={buttonCategorySettings} name= "1y">
             1y
           </button>
-          <div>{categoryComponent[categoryIndex]}</div>
+          {/* <div>{categoryComponent[categoryIndex]}</div> */}
+          <div>
+
+            {categoryIndex == 0 && <CategoryLineGraph_1y {...props}/>}
+            {categoryIndex == 1 && <CategoryLineGraph_1mo {...props}/>}
+            {categoryIndex == 2 && <CategoryLineGraph_1d {...props}/>}
+            {categoryIndex == 3 && <CategoryLineGraph_1y {...props}/>}
+            {categoryIndex == 4 && <CategoryLineGraph_1mo {...props}/>}
+            {categoryIndex == 5 && <CategoryLineGraph_1d {...props}/>}
+          </div>
 
           <CategoryButton/>
-
-          {/* <div>
-          {
-            categoryArray[0] ? (
-              <button onClick={buttonToggleArrayElement} name = "Currency">Currency hide </button>
-              ):(
-                <button onClick={buttonToggleArrayElement} name= "Currency">Currency show </button>
-              )
-            } {
-            categoryArray[1] ? (
-              <button onClick={buttonToggleArrayElement} name = "Smart Contract Platform">Smart Contract Platform hide </button>
-              ):(
-                <button onClick={buttonToggleArrayElement} name= "Smart Contract Platform">Smart Contract Platform show </button>
-              )
-          }{
-            categoryArray[2] ? (
-              <button onClick={buttonToggleArrayElement} name = "Computing">Computing hide </button>
-              ):(
-                <button onClick={buttonToggleArrayElement} name= "Computing">Computing show </button>
-              )
-          }{
-            categoryArray[3] ? (
-              <button onClick={buttonToggleArrayElement} name = "DeFi">DeFi hide </button>
-              ):(
-                <button onClick={buttonToggleArrayElement} name= "DeFi">DeFi show </button>
-              )
-          }{
-            categoryArray[4] ? (
-              <button onClick={buttonToggleArrayElement} name = "Culture & Entertainment">Culture & Entertainment hide </button>
-              ):(
-                <button onClick={buttonToggleArrayElement} name= "Culture & Entertainment">Culture & Entertainment show </button>
-              )
-          }
-          </div> */}
         </div>
         
         <div><FearandGreed/></div>
