3cd1f448f86312fb5795e59f6786fbc506173ff3
Yeeun <im.yeunn@gmail.com>
2022-09-22 13:58:16 +0900

    유망코인

--- src/components/PromisingCoin/CryptoList.js
+++ src/components/PromisingCoin/CryptoList.js
@@ -28,6 +28,14 @@ export default function CryptoCard() {
                     alt="Cryptoimage"
                 />
             </Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                이더리움
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                #이더리움 #기술성
+                </Typography>
+            </CardContent>
         </Grid> 
         <Grid item xs="auto">
             <Card sx={{ maxWidth: 350 }}>
@@ -38,6 +46,14 @@ export default function CryptoCard() {
                     alt="Cryptoimage"
                 />
             </Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                Tezos
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                #Tezos #기술성
+                </Typography>
+            </CardContent>
         </Grid>
         <Grid item xs="auto">
             <Card sx={{ maxWidth: 350 }}>
@@ -48,6 +64,14 @@ export default function CryptoCard() {
                     alt="Cryptoimage"
                 />
             </Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                Lida Dao
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                #Lida Dao #이더리움 #기술성
+                </Typography>
+            </CardContent>
         </Grid>
         <Grid item xs="auto">
             <Card sx={{ maxWidth: 350 }}>
@@ -58,6 +82,14 @@ export default function CryptoCard() {
                     alt="Cryptoimage"
                 />
             </Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                Ultra
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                #Ultra #상승 #기술성
+                </Typography>
+            </CardContent>
         </Grid>
         <Grid item xs="auto">
             <Card sx={{ maxWidth: 350 }}>
@@ -68,6 +100,14 @@ export default function CryptoCard() {
                     alt="Cryptoimage"
                 />
             </Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                Reef
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                #잠재력 #기술성
+                </Typography>
+            </CardContent>
         </Grid>
     </Grid>
   );
