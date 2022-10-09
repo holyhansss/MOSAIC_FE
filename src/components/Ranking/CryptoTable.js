3fba3d7da7a6a163c01b680cc062423c56195a04
Heeeera <chr5487@naver.com>
2022-10-09 22:37:21 +0900

    Fix ranking table

--- src/components/Ranking/CryptoTable.js
+++ src/components/Ranking/CryptoTable.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import CloseIcon from '@mui/icons-material/Close';
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import CloseIcon from "@mui/icons-material/Close";
 import { useNavigate } from "react-router-dom";
 
-export default function CryptoTable({crypto}) {
+export default function CryptoTable({ crypto }) {
   const navigate = useNavigate();
 
   const columns = [
@@ -12,59 +12,56 @@ export default function CryptoTable({crypto}) {
     { field: "id", headerName: "순위" },
     { field: "name", headerName: "이름" },
     { field: "rating", headerName: "등급" },
-    { field: "criteria", headerName: "기준", width: 300 },
-    { field: "tag", headerName: "태그", width: 300 },
-    { field: "promising", headerName: "유망코인", renderCell : (params) => {
-      if(params.row.promising == true ) {
-        return (
-          <AssignmentIcon onClick={() => {
-            navigate("/promising/" + params.row.docid, {
-              state: {
-                id: params.row.docid,
-              },
-            });
-          }}/>
-        )
-      } else {
-        return (
-          <CloseIcon fontSize="small" />
-        )
-      };
-    }},
+    { field: "criteria", headerName: "기준별 점수", width: 300 },
+    { field: "tag", headerName: "태그", width: 400 },
+    {
+      field: "promising",
+      headerName: "유망코인",
+      type: "boolean",
+      renderCell: (params) => {
+        if (params.row.promising == true) {
+          return (
+            <AssignmentIcon
+              onClick={() => {
+                navigate("/promising/" + params.row.docid, {
+                  state: {
+                    id: params.row.docid,
+                  },
+                });
+              }}
+            />
+          );
+        } else {
+          return <CloseIcon fontSize="small" />;
+        }
+      },
+    },
   ];
-  
-  let rows=[];
+
+  let rows = [];
   crypto.map((rank, index) => {
-    if (rank.type == 'coin') {
-      rows.push(
-        {
-          docid : rank.id,
-          id: index+1,
-          name: rank.name,
-          rating: rank.rating,
-          tag: rank.hashtag,
-          promising:rank.promising,
-          criteria: '확장성 : '+ rank.scalability +' 보안성 : '+ rank.security+ ' 탈중앙성 : '+ rank.decentralization ,
-
-        }
-      )
+    if (rank.type == "coin") {
+      rows.push({
+        docid: rank.id,
+        id: index + 1,
+        name: rank.name,
+        rating: rank.rating,
+        tag: rank.hashtag,
+        promising: rank.promising,
+        criteria: `확장성: ${rank.scalability}　탈중앙성: ${rank.decentralization}　보안성: ${rank.security}`,
+      });
     } else {
-      rows.push(
-        {
-          docid : rank.id,
-          id: index+1,
-          name: rank.name,
-          rating: rank.rating,
-          tag: rank.hashtag,
-          promising: rank.promising,
-          criteria: '안전성 : '+ rank.reliability +' 기술성 : '+ rank.technicality+ ' 사업성 : '+ rank.business,
-
-        }
-      )
+      rows.push({
+        docid: rank.id,
+        id: index + 1,
+        name: rank.name,
+        rating: rank.rating,
+        tag: rank.hashtag,
+        promising: rank.promising,
+        criteria: `사업성: ${rank.business}　기술성: ${rank.technicality}　신뢰성: ${rank.reliability}`,
+      });
     }
-  } 
-  );
-
+  });
 
   return (
     <div style={{ width: "100%", marginBottom: "5rem" }}>
