325800c8968b2247162556f50bb87a03a1c8261a
Son388 <21800388@handong.ac.kr>
2022-12-06 15:15:36 +0900

    edit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -93,7 +93,7 @@ const MainPage = ({ result, reports, crypto }) => {
             <Carousel.Item
               style={{ backgroundColor: "#000000", textAlign: "center" }}
             >
-              <Link to="market">
+              <Link to="/market">
                 <img
                   className="mw-100 w-auto"
                   src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/banner%2FMarket.jpg?alt=media&token=50353301-5ea7-467f-a473-7992c2feece3"
@@ -285,7 +285,7 @@ const MainPage = ({ result, reports, crypto }) => {
             <Carousel.Item
               style={{ backgroundColor: "#000000", textAlign: "center" }}
             >
-              <Link to="market">
+              <Link to="/market">
                 <img
                   className="mw-100 w-auto"
                   src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/banner%2FMarket.jpg?alt=media&token=50353301-5ea7-467f-a473-7992c2feece3"
@@ -295,188 +295,181 @@ const MainPage = ({ result, reports, crypto }) => {
             </Carousel.Item>
           </Carousel>
         </div>
-        <MainContainer maxWidth="lg">
-          <Grid container spacing={10} justifyContent="center">
-            <Grid item xs={12}>
+        <Grid container spacing={10} justifyContent="center">
+          <Grid item xs={12}>
+            <img
+              className="w-100"
+              src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/banner%2Fhidden.png?alt=media&token=5ede2848-db74-4880-b929-ab81f5b0bf71"
+              alt="hidden"
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ textAlign: "center" }}>
+            <Typography
+              variant="h6"
+              sx={{ marginBottom: "3rem", lineHeight: 2, fontSize: "80%" }}
+            >
+              <div>
+                MOSAIC은{" "}
+                <span style={{ fontWeight: 900 }}>건전한 암호화폐 투자</span>를
+                선도하기 위해
+              </div>
+              <div>다음과 같은 서비스를 제공합니다.</div>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} container direction="row" spacing={2}>
+            <Grid item xs={4}>
               <img
-                className="w-100"
-                src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/banner%2Fhidden.png?alt=media&token=5ede2848-db74-4880-b929-ab81f5b0bf71"
-                alt="hidden"
+                src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/service_info%2Fundraw_reviews_lp8w.svg?alt=media&token=67ea23a8-ea21-4cdd-9c73-5096420838cb"
+                alt="rating"
+                width="100%"
               />
             </Grid>
-            <Grid item xs={12} sx={{ textAlign: "center" }}>
+            <Grid item xs={8}>
               <Typography
-                variant="h6"
-                sx={{ marginBottom: "3rem", lineHeight: 2, fontSize: "80%" }}
+                align="left"
+                variant="h7"
+                sx={{ margin: "1rem 0", fontWeight: 900, fontSize: "80%" }}
               >
-                <div>
-                  MOSAIC은{" "}
-                  <span style={{ fontWeight: 900 }}>건전한 암호화폐 투자</span>
-                  를 선도하기 위해
-                </div>
-                <div>다음과 같은 서비스를 제공합니다.</div>
+                암호화폐 등급 평가
+              </Typography>
+              <Typography
+                align="left"
+                sx={{ color: "#7F8487", fontSize: "80%" }}
+              >
+                암호화폐가 지닌 내재가치를
+                <br />
+                상세한 기준에 의거하여 평가한 등급을 통해 각 암호화폐의 강약점을
+                파악할 수 있습니다.
               </Typography>
             </Grid>
-            <Grid item xs={12} container direction="row" spacing={2}>
-              <Grid item xs={4}>
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/service_info%2Fundraw_reviews_lp8w.svg?alt=media&token=67ea23a8-ea21-4cdd-9c73-5096420838cb"
-                  alt="rating"
-                  width="100%"
-                />
-              </Grid>
-              <Grid item xs={8}>
-                <Typography
-                  align="left"
-                  variant="h7"
-                  sx={{ margin: "1rem 0", fontWeight: 900, fontSize: "80%" }}
-                >
-                  암호화폐 등급 평가
-                </Typography>
-                <Typography
-                  align="left"
-                  sx={{ color: "#7F8487", fontSize: "80%" }}
-                >
-                  암호화폐가 지닌 내재가치를
-                  <br />
-                  상세한 기준에 의거하여 평가한 등급을 통해 각 암호화폐의
-                  강약점을 파악할 수 있습니다.
-                </Typography>
-              </Grid>
+          </Grid>
+          <Grid item xs={12} container direction="row" spacing={2}>
+            <Grid item xs={4}>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/service_info%2Fundraw_savings_re_eq4w.svg?alt=media&token=5b826551-ac3e-4314-80e4-abd5bbdd96b1"
+                alt="fund"
+                width="100%"
+              />
             </Grid>
-            <Grid item xs={12} container direction="row" spacing={2}>
-              <Grid item xs={4}>
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/service_info%2Fundraw_savings_re_eq4w.svg?alt=media&token=5b826551-ac3e-4314-80e4-abd5bbdd96b1"
-                  alt="fund"
-                  width="100%"
-                />
-              </Grid>
-              <Grid item xs={8}>
-                <Typography
-                  variant="h7"
-                  sx={{ margin: "1rem 0", fontWeight: 900, fontSize: "80%" }}
-                >
-                  펀드 상품
-                </Typography>
-                <br />
-                <Typography
-                  align="left"
-                  variant="h8"
-                  sx={{ color: "#7F8487", fontSize: "80%" }}
-                >
-                  정보 제공을 넘어서 자체적인 투자 철학을 기반으로 펀드를
-                  운용합니다. 투자자는 자신의 성향과 위험선호도에 따라 펀드에
-                  투자할 수 있습니다.
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} container direction="row" spacing={2}>
-              <Grid item xs={4}>
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/service_info%2Fundraw_data_reports_706v.svg?alt=media&token=2b60aa1f-11ce-4c9d-bbe0-7290eff215f1"
-                  alt="report"
-                  width="100%"
-                />
-              </Grid>
-              <Grid item xs={8}>
-                <Typography
-                  variant="h7"
-                  sx={{ margin: "1rem 0", fontWeight: 900, fontSize: "80%" }}
-                >
-                  경제 지표 & 리포트 제공
-                </Typography>
-                <br />
-                <Typography
-                  align="left"
-                  variant="h8"
-                  sx={{ color: "#7F8487", fontSize: "80%" }}
-                >
-                  암호화폐 시장의 현황을 알 수 있도록 공포탐욕지수, 섹터별
-                  인덱스, 벤치마크(주식, 원자재) 등의 거시적 지표와 일간, 주간
-                  리포트를 제공합니다.
-                </Typography>
-              </Grid>
-            </Grid>
-
-            {/* 리포트 */}
-            <Grid item xs={12} sx={{ textAlign: "center" }}>
-              <Divider component="div" role="presentation">
-                <Typography
-                  variant="h6"
-                  sx={{ margin: "0 3.5rem", fontWeight: 900 }}
-                >
-                  최신 리포트
-                </Typography>
-              </Divider>
-            </Grid>
-            <Grid item xs={12}>
-              <div>
-                {result !== undefined ? (
-                  <ReportrecentcardDetail
-                    id={result.id}
-                    title={result.title}
-                    writer={result.writer}
-                    date={moment(result.date).format("YYYY.MM.DD")}
-                    thumbnail={result.thumbnail}
-                    hashtag={result.hashtag}
-                  />
-                ) : null}
-              </div>
-            </Grid>
-            <Grid item xs={12}>
-              <div>
-                {reports.slice(0, 3).map((report, index) => (
-                  <div key={index}>
-                    <ReportlistForm
-                      id={report.id}
-                      title={report.title}
-                      date={moment(report.date).format("YYYY.MM.DD")}
-                      writer={report.writer}
-                      thumbnail={report.thumbnail}
-                    />
-                  </div>
-                ))}
-              </div>
-            </Grid>
-            {/* 유망 코인 */}
-            <Grid item xs={12} sx={{ textAlign: "center" }}>
-              <Divider component="div" role="presentation">
-                <Typography
-                  variant="h5"
-                  sx={{ margin: "0 3.5rem", fontWeight: 900 }}
-                >
-                  최신 유망 코인
-                </Typography>
-              </Divider>
-              <Grid item xs={12}>
-                <Box sx={{ height: "5rem" }} />
-              </Grid>
-              <Grid
-                container
-                direction="row"
-                spacing={5}
-                justifyContent="center"
+            <Grid item xs={8}>
+              <Typography
+                variant="h7"
+                sx={{ margin: "1rem 0", fontWeight: 900, fontSize: "80%" }}
               >
-                {mainCrypto !== null &&
-                  mainCrypto.map((content, index) => (
-                    <Grid
-                      item
-                      xs={12}
-                      md={5}
-                      lg={4}
-                      sx={{ display: "flex", justifyContent: "center" }}
-                    >
-                      <PromisingCard crypto={content} key={index} />
-                    </Grid>
-                  ))}
-              </Grid>
+                펀드 상품
+              </Typography>
+              <br />
+              <Typography
+                align="left"
+                variant="h8"
+                sx={{ color: "#7F8487", fontSize: "80%" }}
+              >
+                정보 제공을 넘어서 자체적인 투자 철학을 기반으로 펀드를
+                운용합니다. 투자자는 자신의 성향과 위험선호도에 따라 펀드에
+                투자할 수 있습니다.
+              </Typography>
             </Grid>
+          </Grid>
+          <Grid item xs={12} container direction="row" spacing={2}>
+            <Grid item xs={4}>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/mosaic-db1e4.appspot.com/o/service_info%2Fundraw_data_reports_706v.svg?alt=media&token=2b60aa1f-11ce-4c9d-bbe0-7290eff215f1"
+                alt="report"
+                width="100%"
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <Typography
+                variant="h7"
+                sx={{ margin: "1rem 0", fontWeight: 900, fontSize: "80%" }}
+              >
+                경제 지표 & 리포트 제공
+              </Typography>
+              <br />
+              <Typography
+                align="left"
+                variant="h8"
+                sx={{ color: "#7F8487", fontSize: "80%" }}
+              >
+                암호화폐 시장의 현황을 알 수 있도록 공포탐욕지수, 섹터별 인덱스,
+                벤치마크(주식, 원자재) 등의 거시적 지표와 일간, 주간 리포트를
+                제공합니다.
+              </Typography>
+            </Grid>
+          </Grid>
+
+          {/* 리포트 */}
+          <Grid item xs={12} sx={{ textAlign: "center" }}>
+            <Divider component="div" role="presentation">
+              <Typography
+                variant="h6"
+                sx={{ margin: "0 3.5rem", fontWeight: 900 }}
+              >
+                최신 리포트
+              </Typography>
+            </Divider>
+          </Grid>
+          <Grid item xs={12}>
+            <div>
+              {result !== undefined ? (
+                <ReportrecentcardDetail
+                  id={result.id}
+                  title={result.title}
+                  writer={result.writer}
+                  date={moment(result.date).format("YYYY.MM.DD")}
+                  thumbnail={result.thumbnail}
+                  hashtag={result.hashtag}
+                />
+              ) : null}
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div>
+              {reports.slice(0, 3).map((report, index) => (
+                <div key={index}>
+                  <ReportlistForm
+                    id={report.id}
+                    title={report.title}
+                    date={moment(report.date).format("YYYY.MM.DD")}
+                    writer={report.writer}
+                    thumbnail={report.thumbnail}
+                  />
+                </div>
+              ))}
+            </div>
+          </Grid>
+          {/* 유망 코인 */}
+          <Grid item xs={12} sx={{ textAlign: "center" }}>
+            <Divider component="div" role="presentation">
+              <Typography
+                variant="h5"
+                sx={{ margin: "0 3.5rem", fontWeight: 900 }}
+              >
+                최신 유망 코인
+              </Typography>
+            </Divider>
             <Grid item xs={12}>
               <Box sx={{ height: "5rem" }} />
             </Grid>
+            <Grid container direction="row" spacing={5} justifyContent="center">
+              {mainCrypto !== null &&
+                mainCrypto.map((content, index) => (
+                  <Grid
+                    item
+                    xs={12}
+                    md={5}
+                    lg={4}
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <PromisingCard crypto={content} key={index} />
+                  </Grid>
+                ))}
+            </Grid>
           </Grid>
-        </MainContainer>
+          <Grid item xs={12}>
+            <Box sx={{ height: "5rem" }} />
+          </Grid>
+        </Grid>
       </Mobile>
     </>
   );
